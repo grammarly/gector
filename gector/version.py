58a63491ebcf5abd61939cb5400e7826ef3cbf7e
Sanjna Kashyap <skashyap@ets.org>
2022-11-03 16:38:34 -0400

    Test diff version

--- gector/version.py
+++ gector/version.py
@@ -3,5 +3,5 @@ This module exists solely for version information so we only have to change it
 in one place. Based on the suggestion `here. <http://bit.ly/16LbuJF>`_
 """
 
-__version__ = "3.0.0"
+__version__ = "1.0.2"
 VERSION = tuple(int(x) for x in __version__.split("."))
