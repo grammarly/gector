afbfab099c56e570ddb546faab490c91b65143e0
Sanjna Kashyap <skashyap@ets.org>
2022-11-03 16:11:00 -0400

    Added version.py file

---
+++ gector/version.py
@@ -0,0 +1,7 @@
+"""
+This module exists solely for version information so we only have to change it
+in one place. Based on the suggestion `here. <http://bit.ly/16LbuJF>`_
+"""
+
+__version__ = "3.0.0"
+VERSION = tuple(int(x) for x in __version__.split("."))
