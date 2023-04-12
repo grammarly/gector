e1ac5263f24ac1f97af2328a54addb3923953c86
Sanjna Kashyap <skashyap@ets.org>
2023-04-12 10:27:28 -0400

    Update version number

--- gector/version.py
+++ gector/version.py
@@ -3,5 +3,5 @@ This module exists solely for version information so we only have to change it
 in one place. Based on the suggestion `here. <http://bit.ly/16LbuJF>`_
 """
 
-__version__ = "2.2.0"
+__version__ = "2.3.0"
 VERSION = tuple(int(x) for x in __version__.split("."))
